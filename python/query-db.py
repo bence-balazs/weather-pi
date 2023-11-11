aed01fa35459ef5de2f0375da88460644182ace3
bence-balazs <bestbence@gmail.com>
2023-11-11 14:52:09 +0100

    initial commit

---
+++ python/query-db.py
@@ -0,0 +1,15 @@
+import psycopg2
+
+def main():
+    conn = psycopg2.connect(
+            database="rpi",
+            host="localhost",
+            user="rpi",
+            password="alma",
+            port="5432",
+        )
+
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM leads")
+    print(cursor.fetchall())
