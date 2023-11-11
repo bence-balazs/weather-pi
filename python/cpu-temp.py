64e729973f047064526311c7b69624f652d24c89
bence-balazs <bestbence@gmail.com>
2023-11-11 13:49:23 +0100

    feat: added formating

--- python/cpu-temp.py
+++ python/cpu-temp.py
@@ -3,6 +3,8 @@ from gpiozero import CPUTemperature
 def main():
     cpu = CPUTemperature()
     print(cpu.temperature)
+    formatted_value = format(cpu.temperature, '.2f')
+    print(formatted_value)
 
 if __name__ == "__main__":
     main()
