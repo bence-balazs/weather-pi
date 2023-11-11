acb80c0b7010b5e1ee5b1c704cfcc99a9045b91a
bence-balazs <bestbence@gmail.com>
2023-11-11 15:42:37 +0100

    initial commit

---
+++ python/test_cpu_insert.py
@@ -0,0 +1,27 @@
+import psycopg2
+
+def main():
+    conn = psycopg2.connect(
+            database="rpi",
+            host="localhost",
+            user="rpi",
+            password="alma",
+            port="5432",
+        )
+
+    cursor = conn.cursor()
+
+    # Table create command
+    sql ='''INSERT INTO cpu_temp (temp)
+            VALUES (33.9)
+        '''
+
+    # Load test data
+    cursor.execute(sql)
+    conn.commit()
+    
+    # Closing the connection
+    conn.close()
+
+if __name__ == "__main__":
+    main()
