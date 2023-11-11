3a5c2a8efd30eef00395503e661ac9920994087e
bence-balazs <bestbence@gmail.com>
2023-11-11 14:13:52 +0100

    feat: removed print

--- python/cpu-temp.py
+++ python/cpu-temp.py
@@ -2,9 +2,8 @@ from gpiozero import CPUTemperature
 
 def main():
     cpu = CPUTemperature()
-    print(cpu.temperature)
-    formatted_value = format(cpu.temperature, '.2f')
-    print(formatted_value)
+    formatted_value = format(cpu.temperature, '.1f')
+    
 
 if __name__ == "__main__":
     main()
